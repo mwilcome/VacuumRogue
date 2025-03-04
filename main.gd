ca03c62327591786b9ae025c02f8c9d2ff65a377
Mike Wilcome <mwilcomesc@gmail.com>
2025-03-04 12:12:27 -0500

    Added som additonal trash

--- main.gd
+++ main.gd
@@ -6,12 +6,16 @@ extends Node3D
 func _ready():
 	spawn_trash()
 
+func _process(_delta):
+	if $TrashContainer.get_child_count() == 0:
+		spawn_trash()
+
 func spawn_trash():
 	for i in range(trash_count):
 		var trash = trash_scene.instantiate()
 		$TrashContainer.add_child(trash)
 		trash.position = Vector3(
-			randf_range(-4.5, 4.5),  # X: Within floor bounds (10 wide)
-			0.1,                    # Y: On floor surface
-			randf_range(-4.5, 4.5)  # Z: Within floor bounds (10 deep)
+			randf_range(-4.5, 4.5),
+			0.1,
+			randf_range(-4.5, 4.5)
 		)
